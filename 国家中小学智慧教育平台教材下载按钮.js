04b5664efc566850604aeae4d2ff7cba991d2777
300g-BoHeYou <95151930+300g-BoHeYou@users.noreply.github.com>
2023-07-16 10:14:46 +0800

    更新到2.2版本

--- 国家中小学智慧教育平台教材下载按钮.js
+++ 国家中小学智慧教育平台教材下载按钮.js
@@ -1,28 +1,34 @@
 // ==UserScript==
 // @name         国家中小学智慧教育平台教材下载按钮
-// @namespace    http://tampermonkey.net/
-// @version      2.1
+// @namespace    https://greasyfork.org/zh-CN/scripts/450700
+// @version      2.2
 // @description  用于下载国家中小学智慧教育平台的教材
 // @author       300g-BoHeYou
 // @match        https://www.zxx.edu.cn/tchMaterial/*
+// @match        https://basic.smartedu.cn/tchMaterial/*
 // @icon         https://www.zxx.edu.cn/favicon.ico
 // @license MIT
 // @grant        GM_download
-// @grant        GM_addElement
 // @grant        GM_log
 
 // ==/UserScript==
 
 (function() {
     'use strict';
-    GM_addElement(document.body,"input", {type:"button",value:"下载pdf",title:"download",class:"downloadbtn"});
+    setTimeout(function(){
+        var download = document.getElementsByClassName("index-module_info_evO1d")[0];
+        var download_div = document.createElement("div");
+        download.appendChild(download_div);
+        download_div.style = 'width:150px;text-align:right;';
+        download_div.innerHTML = '<span class="downloadbtn" style="color:white;padding:10px 20px;border-radius:10px;background:red">下载pdf</span>';
 
-    document.getElementsByClassName("downloadbtn")[0].onclick = function(){
-        GM_download({
-            url:performance.getEntriesByType('resource').filter(entry =>{return /.*pdf\.pdf/.test(entry.name);})[0].name,
-            name:document.title.split('·')[1]+".pdf",
-            saveAs: true
-        })
-    };
+        document.getElementsByClassName("downloadbtn")[0].onclick = function(){
+            GM_download({
+                url:performance.getEntriesByType('resource').filter(entry =>{return /.*pdf\.pdf/.test(entry.name);})[0].name,
+                name:document.title.split('·')[1]+".pdf",
+                saveAs: true
+            })
+        };
+    },2000);
 
 })();
